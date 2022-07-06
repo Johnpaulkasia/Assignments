7faba1dbb104994296b338c6f5cda79765593f09
Johnpaulkasia <90444717+Johnpaulkasia@users.noreply.github.com>
2022-07-06 08:14:27 +0200

    Add files via upload

---
+++ Insertion.sql
@@ -0,0 +1,39 @@
+INSERT INTO Employer
+(FullName,JoiningDate,CurrentPosition,Department,AssignedProject)
+VALUES
+('John Paul', '2000-02-14','Manager','Finance','c++');
+
+INSERT INTO Employer
+(FullName,JoiningDate,CurrentPosition,Department,AssignedProject)
+VALUES
+('peter Micheal', '2002-06-30','Assistant Manager','HR','c#');
+
+INSERT INTO Employer
+(FullName,JoiningDate,CurrentPosition,Department,AssignedProject)
+VALUES
+('Issac Newton', '2004-04-22','Intern','Travel','Java');
+
+INSERT INTO Employer
+(FullName,JoiningDate,CurrentPosition,Department,AssignedProject)
+VALUES
+('Hulk Holgan', '2006-08-18','General Overseer', 'Software and Data','Python');
+
+INSERT INTO Services
+(Name,Category,Size,NumberOfInstallments)
+VALUES
+('Grand theft auto', 'A',4055,1000);
+
+INSERT INTO Services
+(Name,Category,Size,NumberOfInstallments)
+VALUES
+('Ultimate Fighter', 'B',2044,2000);
+
+INSERT INTO Services
+(Name,Category,Size,NumberOfInstallments)
+VALUES
+('Fifa', 'C',1024,4000);
+
+INSERT INTO Services
+    (Name,Category,Size,NumberOfInstallments)
+VALUES
+('Call Of Duty', 'D',6240,8000);
